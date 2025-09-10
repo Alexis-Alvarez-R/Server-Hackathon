4a831732e285d24709b81c6e3cdc8ea8c75950a6
Alexis-Alvarez-R <ramirez151202@gmail.com>
2025-09-09 23:45:36 -0600

    conexion a la BD

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,1 +1,10 @@
 //CONEXION A LA BD
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config(); // esto carga el .env
+
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
 //MAIN (PUNTO DE ENTRADA)
-console.log("petitaaaaa pereeeeeez");
+
+import express from "express";
+import cors from "cors";
+import { supabase } from "./data-source.js"; //siempre pon la extension .js
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Obtener todas las aves
+app.get("/aves", async (req, res) => {
+  const { data, error } = await supabase.from("aves").select(`
+      ave_id,
+      nombre_comun,
+      nombre_cientifico,
+      descripcion,
+      tamano,
+      dieta,
+      habitat,
+      familias(nombre),
+      estados_conservacion(nombre)
+    `);
+
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
+});
