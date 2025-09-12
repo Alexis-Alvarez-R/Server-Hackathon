5c782751e552a32675ba63bfaa3afc706cbfcd20
Alexis-Alvarez-R <ramirez151202@gmail.com>
2025-09-12 16:43:26 -0600

    nuevo campo en /aves

--- src/controllers/aves.controller.ts
+++ src/controllers/aves.controller.ts
@@ -1,10 +1,8 @@
 import { supabase } from "../data-source.js"; //siempre pon la extension .js
 import type { Request, Response } from "express";
 
-
-export const getAves= async(req: Request, res:Response)=> {
-
-    const { data, error } = await supabase.from("aves").select(`
+export const getAves = async (req: Request, res: Response) => {
+  const { data, error } = await supabase.from("aves").select(`
       ave_id,
       nombre_comun,
       nombre_cientifico,
@@ -12,11 +10,11 @@ export const getAves= async(req: Request, res:Response)=> {
       tamano,
       dieta,
       habitat,
+      url_img,
       familias(nombre),
       estados_conservacion(nombre)
     `);
 
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
-}
-
+};
