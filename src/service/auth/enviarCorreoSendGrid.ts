7ff9c3611db0a9162f1d5648996fa810b9264094
Somarriba hi <128437648+somarriba02polz@users.noreply.github.com>
2025-09-28 21:04:55 -0600

    fix rutas redirect email

--- src/service/auth/enviarCorreoSendGrid.ts
+++ src/service/auth/enviarCorreoSendGrid.ts
@@ -1,5 +1,6 @@
 import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
+import { webUrl } from "../../index.js";
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ export async function enviarCorreo(email: string, token: string) {
       from: "rumbonicaapp@gmail.com",
       subject: "Bienvenido a RumboNica",
       html: `<p>Haz click en el link para verificar tu cuenta:</p>
-                   <a href="http://localhost:${process.env.PORT}/auth/verificartoken?tokenVerificacion=${token}">Verificar correo</a>`,
+                   <a href="${webUrl}/auth/verificartoken?tokenVerificacion=${token}">Verificar correo</a>`,
     };
     await sgMail.send(msg);
   } catch (error) {
